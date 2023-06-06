6316281a49eb7b8e69704d4a9c4a1099a23830ed
qdieu <navvy3@gmail.com>
2023-06-06 23:13:50 +0200

    feat: change logic price section

--- src/components/ServicesAndPricesSection/ServicesAndPricesSection.jsx
+++ src/components/ServicesAndPricesSection/ServicesAndPricesSection.jsx
@@ -1,123 +1,183 @@
+import { useState } from 'react'
 import { Enroll } from '../Enroll'
 import s from './ServicesAndPricesSection.module.scss'
+import cn from 'classnames';
+
+const sectionInfo = [
+  {
+    title: 'Стрижка',
+    text: 'Профессиональная стрижка для кошек и собак любой породы',
+    src: "images/serv1.PNG",
+    textContent: `Профессиональная стрижка избавит от линьки, колтунов
+    и загрязнений, а так же сделает питомца более привлекательным.
+    У нас стригут длинношерстных и короткошёрстных четвероногих.
+    Мастера могут не тольно подровнять, но и сделать креативные
+    модельные стрижки различной сложности.
+    Длительность процедуры: для кошек - от 40 до 80 минут,
+    для собак - от 50 до 100 минут.  
+    `,
+    servicePrice: [
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2300',
+        dog: '2000',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2000',
+        dog: '2500',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '500',
+        dog: '650',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '3500',
+        dog: '',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '',
+        dog: '200',
+      },
+    ]
+  },
+  {
+    title: 'Гигиенические процедуры',
+    text: `Чистка ушей, глаз, зубов, стрижка когтей, экспресс-линька и многое другое`,
+    src: "images/serv2.PNG",
+    textContent: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, perferendis officia. Quam voluptas molestiae cupiditate, corrupti quo culpa nisi nostrum est! Iure nam veniam eum ipsum vel voluptates cupiditate alias?`,
+    servicePrice: [
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2300',
+        dog: '2000',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2000',
+        dog: '2500',
+      },
+    ]
+  },
+  {
+    title: 'SPA-комплексы',
+    text: `Премиальные маски для защиты и глубокого питания кожи и шерсти`,
+    src: "images/serv3.PNG",
+    textContent: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, perferendis officia. Quam voluptas molestiae cupiditate, corrupti quo culpa nisi nostrum est! Iure nam veniam eum ipsum vel voluptates cupiditate alias?`,
+    servicePrice: [
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2300',
+        dog: '2000',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2000',
+        dog: '2500',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '500',
+        dog: '650',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '3500',
+        dog: '',
+      },
+    ]
+    
+  },
+  {
+    title: 'Окрашивание',
+    text: `Окрашивание шерсти в различные цвета специальной краской,окрашивание узором`,
+    src: "images/serv4.PNG",
+    textContent: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, perferendis officia. Quam voluptas molestiae cupiditate, corrupti quo culpa nisi nostrum est! Iure nam veniam eum ipsum vel voluptates cupiditate alias?`,
+    servicePrice: [
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2300',
+        dog: '2000',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '2000',
+        dog: '2500',
+      },
+      {
+        name: 'Гигиеническая стрижка',
+        cat: '500',
+        dog: '650',
+      },
+    ]
+    
+  }
+]
 
 const ServicesAndPricesSection = () => {
+  const [selectedSection, setSelectedSection] = useState('Стрижка');
+
+  function handleClickSection(value) {
+    setSelectedSection(value);
+  }
+
   return (
     <section id="services" className={s.servicesAndPricesSection}>
       <div className={s.contentWrapper}>
         <h2>Услуги и цены</h2>
         <div className={s.blocks}>
-          <div className={s.wrapper}>
-            <img src="images/serv1.PNG"></img>
-            <h3>Стрижка</h3>
-            <p>
-              Профессиональная
-              <br />
-              стрижка для кошек
-              <br />и собак любой породы
-            </p>
-          </div>
-
-          <div className={s.wrapper}>
-            <img src="images/serv2.PNG"></img>
-            <h3>Гигиенические процедуры</h3>
-            <p>
-              Чистка ушей, глаз,
-              <br />
-              зубов, стрижка когтей,
-              <br />
-              экспресс-линька
-              <br />и многое другое
-            </p>
-          </div>
-
-          <div className={s.wrapper}>
-            <img src="images/serv3.PNG"></img>
-            <h3>SPA-комплексы</h3>
-            <p>
-              Премиальные маски
-              <br />
-              для защиты и глубокого
-              <br />
-              питания кожи и шерсти
-            </p>
-          </div>
-
-          <div className={s.wrapper}>
-            <img src="images/serv4.PNG"></img>
-            <h3>Окрашивание</h3>
-            <p>
-              Окрашивание шерсти
-              <br />в различные цвета
-              <br />
-              специальной краской,
-              <br />
-              окрашивание узором
-            </p>
-          </div>
+          {sectionInfo.map(section => (
+            <div
+              className={cn(s.wrapper, {
+                [s.activeWrapper]: section.title === selectedSection
+              })}
+              onClick={() => handleClickSection(section.title)}
+            >
+              <img src={section.src} />
+              <h3>{section.title}</h3>
+              <p>{section.text}</p>
+            </div>
+          ))}
         </div>
-        <div className={s.changeInfo}>
-          <div className={s.info}>
-            <h3>Стрижка</h3>
-            <p>
-              Профессиональная стрижка избавит от линьки, колтунов
-              <br />и загрязнений, а так же сделает питомца более
-              привлекательным.
-              <br />У нас стригут длинношерстных и короткошёрстных четвероногих.
-              <br />
-              Мастера могут не тольно подровнять, но и сделать креативные
-              <br />
-              модельные стрижки различной сложности.
-              <br />
-              <br />
-              Длительность процедуры: для кошек - от 40 до 80 минут,
-              <br />
-              для собак - от 50 до 100 минут.
-            </p>
-            <Enroll />
-          </div>
-
-          <div className={s.table}>
-            <table>
-              <thead>
-                <tr>
-                  <th scope="col">Услуги</th>
-                  <th className={s.elem} scope="col">Кошки</th>
-                  <th className={s.elem} scope="col">Собаки</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Гигиеническая стрижка</td>
-                  <td className={s.elem} >от 2300 р</td>
-                  <td className={s.elem} >от 2000 р</td>
-                </tr>
-                <tr>
-                  <td>Креативный груминг</td>
-                  <td className={s.elem} >от 2000 р</td>
-                  <td className={s.elem} >от 2500 р</td>
-                </tr>
-                <tr>
-                  <td>Модельная стрижка головы</td>
-                  <td className={s.elem} >от 500 р</td>
-                  <td className={s.elem} >от 650 р</td>
-                </tr>
-                <tr>
-                  <td>Стрижка "Лев"</td>
-                  <td className={s.elem} >от 3500 р</td>
-                  <td className={s.elem} >-</td>
-                </tr>
-                <tr>
-                  <td>Пуделиные лапки</td>
-                  <td className={s.elem} >-</td>
-                  <td className={s.elem} >от 200 р</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+        {sectionInfo.map(section => 
+          section.title === selectedSection ?
+          <div className={s.changeInfo}>
+            <div className={s.info}>
+              <h3>{section.title}</h3>
+              <p>
+                {section.textContent}
+              </p>
+              <Enroll />
+            </div>
+            <div className={s.table}>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Услуги</th>
+                    <th className={s.elem} scope="col">Кошки</th>
+                    <th className={s.elem} scope="col">Собаки</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  { section?.servicePrice &&
+                    section.servicePrice.map(service => (
+                      <tr>
+                        <td>{service.name}</td>
+                        <td className={s.elem}>{service.cat ? `от ${service.cat} р` : '-'}</td>
+                        <td className={s.elem}>{service.dog ? `от ${service.dog} р` : '-'}</td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div> : null
+        )}
       </div>
-    </section>
+    </section >
   )
 }
 
